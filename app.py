b085d0c538cfd10d1b84caf5249cd710d6ceeb2a
Tamer Tahamoqa <tamerthamoqa@gmail.com>
2020-08-27 15:53:49 +0300

    Add prediction value logging in app.py

--- app.py
+++ app.py
@@ -16,11 +16,13 @@ def scale(payload):
     LOG.info(f"Scaling Payload: \n{payload}")
     scaler = StandardScaler().fit(payload.astype(float))
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
+
     return scaled_adhoc_predict
 
 @app.route("/")
 def home():
     html = f"<h3>Sklearn Prediction Home</h3>"
+
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
@@ -63,6 +65,8 @@ def predict():
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
+    LOG.info(f"Output prediction value: {prediction}")
+
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
