ad08c5396fd34a093f5d82eb647a0319c10c4b5c
Tamer Tahamoqa <tamerthamoqa@gmail.com>
2020-08-28 16:11:46 +0300

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
-
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=tamerthamoqa/udacity-devops-project4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run udacity-devops-project4 \
+    --image=$dockerpath \
+    --port=80 \
+    --labels app=udacity-devops-project4  # Kubernetes label must be DNS-compliant
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity-devops-project4 8000:80
