65f0f6b9b46bd8dfd92779ddc68e5bf06cc513e1
Tamer Tahamoqa <tamerthamoqa@gmail.com>
2020-08-28 16:11:29 +0300

    Change docker image name to be DNS-compliant for use with Kubernetes

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,14 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-dockerpath=tamerthamoqa/udacity_devops_project4
+dockerpath=tamerthamoqa/udacity-devops-project4
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 docker login --username=tamerthamoqa
-docker tag udacity_devops_project4 tamerthamoqa/udacity_devops_project4
+docker tag udacity-devops-project4 tamerthamoqa/udacity-devops-project4
 
 # Step 3:
 # Push image to a docker repository
-docker push tamerthamoqa/udacity_devops_project4
+docker push tamerthamoqa/udacity-devops-project4
